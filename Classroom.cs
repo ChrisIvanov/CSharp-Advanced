eaff2ac0ac509c346a0bd96f042a70963c5e48d8
Chris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2020-10-25 20:03:17 +0200

    Add files via upload

---
+++ Classroom.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        private int capacity;
+        public Classroom(int capacity)
+        {
+            students = new List<Student>();
+            Capacity = capacity;
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return students.Count;
+            }
+            set
+            {
+                Count = value;
+            }
+
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            if (capacity > this.students.Count)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+
+            Student student = this.students.Find(x => x.FirstName == firstName && x.LastName == lastName);
+            if (this.students.Contains(student))
+            {
+                this.students.Remove(student);
+                return $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+
+
+            return $"Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> bySubject = this.students.FindAll(x => x.Subject == subject).ToList();
+
+            if (bySubject.Count > 0)
+            {
+                string subjectInfo = String.Empty;
+                  subjectInfo += ($"Subject: {subject}\nStudent:");
+
+                foreach (var student in bySubject)
+                {
+                    subjectInfo += ($"\n{student.FirstName} {student.LastName}");
+                }
+                return subjectInfo;
+            }
+
+            return "No students enrolled for the subject";
+
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.students.Count;
+        }
+
+        public string GetStudent(string firstName, string lastName)
+        {
+            Student student = this.students.Find(x => x.FirstName == firstName && x.LastName == lastName);
+
+            return student.ToString();
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassroomProject
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Classroom classroom = new Classroom(5);
+
+            Student studentOne = new Student("Pesho", "Peshov", "Art");
+            Student studentTwo = new Student("Pesho", "Goshov", "Art");
+            
+            classroom.RegisterStudent(studentOne);
+            classroom.RegisterStudent(studentTwo);
+
+            Console.WriteLine(classroom.GetStudentsCount());
+
+            Student studentThree = new Student("Kristian", "Ivanov", "Art");
+            Student studentFour = new Student("Ivan", "Ivanov", "Art");
+
+            Console.WriteLine(classroom.RegisterStudent(studentThree));
+            Console.WriteLine(classroom.RegisterStudent(studentFour));
+            
+
+            Console.WriteLine(classroom.GetStudent("Kristian", "Ivanov"));
+            Console.WriteLine(classroom.GetStudent("Ivan", "Ivanov"));
+
+            Console.WriteLine(classroom.GetStudentsCount());
+
+
+            Console.WriteLine(classroom.DismissStudent("Pesho", "Goshov"));
+
+            Console.WriteLine(classroom.Count);
+
+            Console.WriteLine(classroom.GetSubjectInfo("Art")); 
+
+        }
+    }
+}
